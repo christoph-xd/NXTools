b0b599dade71bfc707b60cc1d7d1a9361d2548a3
BlumenKopf <72992408+BlumenKopf@users.noreply.github.com>
2021-03-24 20:41:21 +0100

    Create open_workspace.py

---
+++ src/open_workspace.py
@@ -0,0 +1,25 @@
+﻿# NX 1911
+# Journal created by brandauc on Wed Apr 22 13:07:48 2020 Mitteleuropäische Sommerzeit
+
+#
+import math
+import NXOpen
+import shutil
+import os
+import getpass
+import time
+import subprocess
+
+def main() : 
+    theSession  = NXOpen.Session.GetSession()
+    workPart = theSession.Parts.Work
+    displayPart = theSession.Parts.Display
+    
+    full_path = theSession.Parts.Work.FullPath
+    partname = os.path.basename(full_path)
+    pathname = os.path.dirname(full_path)
+
+    os.startfile(pathname)
+
+if __name__ == '__main__':
+    main()
