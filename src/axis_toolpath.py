2afe006791d9ca01f0f023e530a6be56c595e9f4
Christoph Brandau <c.brandau91@gmail.com>
2023-03-09 20:08:55 +0100

    --> creat axis only start and end

--- src/axis_toolpath.py
+++ src/axis_toolpath.py
@@ -13,6 +13,7 @@ class CreateAxis:
         self.workPart = self.theSession.Parts.Work
         self.unit = self.workPart.UnitCollection.FindObject("MilliMeter")
         self.flagFirstMotion = False
+        self.flagLastMotion = False
 
     def calculate_endpoint(
         self, point: NXOpen.Point3d, vector: NXOpen.Vector3d
@@ -22,7 +23,7 @@ class CreateAxis:
         y = point.Y + length * vector.Y
         z = point.Z + length * vector.Z
         endpoint = NXOpen.Point3d(x, y, z)
-        
+
         return endpoint
 
     def create_point(self, points: NXOpen.Point3d):
@@ -31,9 +32,8 @@ class CreateAxis:
 
     def create_vector(self, vector: NXOpen.Vector3d, startPoint: NXOpen.Point3d):
         endPoint = self.calculate_endpoint(startPoint, vector)
-        v = self.workPart.Datums.CreateFixedDatumAxis(startPoint, endPoint)
-        lw(v)
- 
+        self.workPart.Datums.CreateFixedDatumAxis(startPoint, endPoint)
+
     def linear_motion(self, path: NXOpen.CAM.Path, toolPathEvent: NXOpen.CAM.PathEvent):
         pathLinearMotion: NXOpen.CAM.PathLinearMotion = path.GetLinearMotion(
             toolPathEvent
@@ -44,7 +44,6 @@ class CreateAxis:
 
         self.create_point(endPoints)
         self.create_vector(toolAxis, endPoints)
-        self.flagFirstMotion = True
 
     def circular_motion(
         self, path: NXOpen.CAM.Path, toolPathEvent: NXOpen.CAM.PathEvent
@@ -82,138 +81,226 @@ class CreateAxis:
                 NXOpen.NXMessageBox.DialogType.Error,
                 "No Operation was selected!",
             )
-
+        selected = self.theUI.SelectionManager.GetSelectedTaggedObject
         for i in range(num):
-            objects1[i] = self.theUI.SelectionManager.GetSelectedTaggedObject(i)
+            objects1[i] = selected(i)
             object: NXOpen.CAM.Operation = objects1[i]
             path: NXOpen.CAM.Path = object.GetPath()
             numberOfToolpathEvents: int = path.NumberOfToolpathEvents
 
-            for j in reversed(range(numberOfToolpathEvents)):
-                j += 1
-                camPathToolpathEventType: NXOpen.CAM.CamPathToolpathEventType = (
-                    path.GetToolpathEventType(j)
-                )
+            while not self.flagFirstMotion and not self.flagLastMotion:
+                for j in range(numberOfToolpathEvents):
+                    j += 1
+                    camPathToolpathEventType: NXOpen.CAM.CamPathToolpathEventType = (
+                        path.GetToolpathEventType(j)
+                    )
 
-                if (
-                    camPathToolpathEventType
-                    == NXOpen.CAM.CamPathToolpathEventType.Motion
-                ):
-                    # lw("Its Motion")
-                    toolPathEvent = path.GetToolpathEvent(j)
-                    (
-                        _,
-                        camPathMotionType,
-                        camPathMotionShapeType,
-                    ) = path.IsToolpathEventAMotion(toolPathEvent)
+                    if (
+                        camPathToolpathEventType
+                        == NXOpen.CAM.CamPathToolpathEventType.Motion
+                    ):
+                        # lw("Its Motion")
+                        self.flagFirstMotion = True
+                        toolPathEvent = path.GetToolpathEvent(j)
+                        (
+                            _,
+                            camPathMotionType,
+                            camPathMotionShapeType,
+                        ) = path.IsToolpathEventAMotion(toolPathEvent)
 
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.FirstCut:
-                        # lw("Its FirstCut")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Rapid:
-                        # lw("Its Rapid")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.From:
-                        # lw("Its From")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Approach:
-                        # lw("Its Approach")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Engage:
-                        # lw("Its Engage")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Cut:
-                        # lw("Its Cut")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.SideCut:
-                        # lw("Its SideCut")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Stepover:
-                        # lw("Its Stepover")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.InternalLift:
-                        # lw("Its InternalLift")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Retract:
-                        # lw("Its Retract")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Traversal:
-                        # lw("Its Traversal")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Gohome:
-                        # lw("Its Gohome")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Return:
-                        # lw("Its Return")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Departure:
-                        # lw("Its Departure")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Cycle:
-                        # lw("Its Cycle")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
-                    if camPathMotionType == NXOpen.CAM.CamPathMotionType.Undefined:
-                        # lw("Undefined")
-                        self.check_motion_shape(
-                            camPathMotionShapeType, path, toolPathEvent
-                        )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.FirstCut:
+                            # lw("Its FirstCut")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Rapid:
+                            # lw("Its Rapid")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.From:
+                            # lw("Its From")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Approach:
+                            # lw("Its Approach")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Engage:
+                            # lw("Its Engage")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Cut:
+                            # lw("Its Cut")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.SideCut:
+                            # lw("Its SideCut")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Stepover:
+                            # lw("Its Stepover")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if (
+                            camPathMotionType
+                            == NXOpen.CAM.CamPathMotionType.InternalLift
+                        ):
+                            # lw("Its InternalLift")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Retract:
+                            # lw("Its Retract")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Traversal:
+                            # lw("Its Traversal")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Gohome:
+                            # lw("Its Gohome")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Return:
+                            # lw("Its Return")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Departure:
+                            # lw("Its Departure")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Cycle:
+                            # lw("Its Cycle")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Undefined:
+                            # lw("Undefined")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
 
-                if (
-                    camPathToolpathEventType
-                    == NXOpen.CAM.CamPathToolpathEventType.LevelMarker
-                ):
-                    lw("Its Level Marker")
+                    if self.flagFirstMotion:
+                        break
+                for j in reversed(range(numberOfToolpathEvents)):
+                    j += 1
+                    camPathToolpathEventType: NXOpen.CAM.CamPathToolpathEventType = (
+                        path.GetToolpathEventType(j)
+                    )
 
-                if (
-                    camPathToolpathEventType
-                    == NXOpen.CAM.CamPathToolpathEventType.System
-                ):
-                    lw("Its System")
+                    if (
+                        camPathToolpathEventType
+                        == NXOpen.CAM.CamPathToolpathEventType.Motion
+                    ):
+                        # lw("Its Motion")
+                        self.flagLastMotion = True
+                        toolPathEvent = path.GetToolpathEvent(j)
+                        (
+                            _,
+                            camPathMotionType,
+                            camPathMotionShapeType,
+                        ) = path.IsToolpathEventAMotion(toolPathEvent)
 
-                if camPathToolpathEventType == NXOpen.CAM.CamPathToolpathEventType.Ude:
-                    lw("Its UDE")
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.FirstCut:
+                            # lw("Its FirstCut")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Rapid:
+                            # lw("Its Rapid")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.From:
+                            # lw("Its From")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Approach:
+                            # lw("Its Approach")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Engage:
+                            # lw("Its Engage")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Cut:
+                            # lw("Its Cut")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.SideCut:
+                            # lw("Its SideCut")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Stepover:
+                            # lw("Its Stepover")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if (
+                            camPathMotionType
+                            == NXOpen.CAM.CamPathMotionType.InternalLift
+                        ):
+                            # lw("Its InternalLift")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Retract:
+                            # lw("Its Retract")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Traversal:
+                            # lw("Its Traversal")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Gohome:
+                            # lw("Its Gohome")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Return:
+                            # lw("Its Return")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Departure:
+                            # lw("Its Departure")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Cycle:
+                            # lw("Its Cycle")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
+                        if camPathMotionType == NXOpen.CAM.CamPathMotionType.Undefined:
+                            # lw("Undefined")
+                            self.check_motion_shape(
+                                camPathMotionShapeType, path, toolPathEvent
+                            )
 
-                if (
-                    camPathToolpathEventType
-                    == NXOpen.CAM.CamPathToolpathEventType.Marker
-                ):
-                    lw("Its Marker")
-                
-                if self.flagFirstMotion:
-                    break
-            
+                    if self.flagLastMotion:
+                        break
+
 
 if __name__ == "__main__":
-    lw(sys.version)
     inistance = CreateAxis()
     inistance.create_axis()
